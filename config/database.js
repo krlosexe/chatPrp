2fb8f37f2e89671abe976004765cb50ca4a46620
Carlos Cardenas <carloscardenas@Carloss-iMac.local>
2020-05-14 15:27:08 -0500

    TEST

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 function ConnecMongo() {
     
-    const uri = "mongodb://krlsoexe:ad17urca@31.220.60.218/:27017/chat";
+    const uri = "mongodb://krlsoexe:ad17urca@127.0.0.1/:27017/chat";
 
     let mongoOptions = {
         useNewUrlParser: true, 
